1d1649911282bdfeca3578f90c707606865a2179
unknown <fariasmauricio11@outlook.com>
2025-06-24 11:44:46 -0300

    Update files

--- app/page.tsx
+++ app/page.tsx
@@ -1,141 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Home from "./components/home";
 
-export default function Home() {
-  const [isTicketModalOpen, useIsTicketModalOpen] = useState(false);
-
-  const handleTicketModal = () => {
-    useIsTicketModalOpen(true);
-  };
+export default function Main() {
+  const queryClient = new QueryClient();
 
   return (
-    <main>
-      <div className="grid place-items-center">
-        {/* MODAL */}
-
-        {isTicketModalOpen && (
-          <form className="pt-10 grid gap-y-1 w-[600px] h-[400px] bg-amber-50 text-black rounded-xl p-4 absolute -translate-y-[50%] top-[50%]">
-            <button
-              onClick={() => useIsTicketModalOpen(false)}
-              className="cursor-pointer absolute top-2 right-2 bg-red-400 font-bold pt-1 rounded-full w-fit grid place-items-center px-2"
-            >
-              fechar
-            </button>
-
-            <div className="flex justify-between items-center">
-              <p>
-                <strong>ID</strong>:{" "}
-                <span className="text-sm bg-gray-500 text-white p-1 rounded-2xl ">
-                  0001
-                </span>
-              </p>
-
-              <div className="flex items-center">
-                <p className="mr-4">
-                  <strong>Data</strong>:
-                </p>
-                <select className="border" name="" id="">
-                  <label className="">data</label>
-                  <option value="">Hoje</option>
-                  <option value="">Amanhã</option>
-                  <option value="">Escolher Data</option>
-                </select>
-              </div>
-            </div>
-            <div className="flex items-center justify-between">
-              <div>
-                <label htmlFor="" className="mr-2">
-                  <strong>Ticket</strong>:
-                </label>
-                <input type="text" className="border-b" />
-              </div>
-
-              <div className="flex items-center">
-                <p className="mr-4">
-                  <strong>Tipo</strong>:
-                </p>
-                <select className="border " name="" id="">
-                  <label className="">tipo</label>
-                  <option value="">Padrão</option>
-                  <option value="">Plano de Aula</option>
-                  <option value="">Pós Graduação</option>
-                </select>
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div>
-                <label htmlFor="" className="mr-2">
-                  <strong>Código</strong>:
-                </label>
-                <input className="border-b" type="text" />
-              </div>
-
-              <div className="flex items-center">
-                <p className="mr-4">
-                  <strong>STATUS</strong>:
-                </p>
-                <select className="border " name="" id="">
-                  <label className="">status</label>
-                  <option value="">Ativa</option>
-                  <option value="">Aprovada</option>
-                  <option value="">Desenvolvimento</option>
-                  <option value="">Pendente</option>
-                  <option value="">Salvo Local</option>
-                </select>
-              </div>
-            </div>
-
-            <div className="flex gap-4 mt-4">
-              <p>
-                <strong>LINK</strong>:{" "}
-                <span className="text-sm w-fit bg-gray-500 text-white rounded-2xl p-0.5 px-2">
-                  <a
-                    href="https://trilhaaprendizagem.uniasselvi.com.br/164489_experiencia_profissional_temas_tranversais/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    164489_experiencia_profissional_temas_tranversais
-                  </a>
-                </span>
-              </p>
-              <div className="bg-blue-500 h-fit text-white px-2 rounded-xl text-sm cursor-pointer">
-                Editar link
-              </div>
-            </div>
-
-            <button className="cursor-pointer bg-gray-500 text-white rounded-2xl font-bold text-xl">
-              Adicionar
-            </button>
-          </form>
-        )}
-
-        <h3 className="text-3xl font-bold text-center mt-14">
-          Tabela de Socilitações de Chamados de Alterações
-        </h3>
-
-        <button
-          onClick={handleTicketModal}
-          className="cursor-pointer mt-12 bg-white p-4 font-bold text-black text-xl rounded-3xl"
-        >
-          Adicionar nova solicitação
-        </button>
-
-        <table className="mt-12">
-          <thead>
-            <th className="border px-2 py-2">ID</th>
-            <th className="border px-4 py-2">DATA</th>
-            <th className="border px-2 py-2">SEMANA</th>
-            <th className="border px-12 py-2">TICKET</th>
-            <th className="border px-6 py-2">CÓDIGO</th>
-            <th className="border px-10 py-2">STATUS</th>
-            <th className="border px-6 py-2">TIPO</th>
-            <th className="border px-12 py-2">LINK</th>
-          </thead>
-          <tbody></tbody>
-        </table>
-      </div>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   );
 }
--- app/pages/api/route.ts
+++ app/pages/api/route.ts
@@ -1,100 +1,118 @@
 "use server";
 
 import { neon } from "@neondatabase/serverless";
-import { chamadoPlaceHolder } from "../../placeholder-data";
 import { NextResponse } from "next/server";
 
 const sql = neon(process.env.DATABASE_URL!);
 
-let isSeeded = false;
-
-export async function POST(request: Request) {
+async function testConnection() {
   try {
-    const placeholderData = "10/11/22";
-    const placeholderTicket = "164489164489";
-
-    const result = await sql`
-      INSERT INTO chamados (data, ticket) 
-      VALUES (${placeholderData}, ${placeholderTicket})
-      RETURNING *
-    `;
-
-    return NextResponse.json({ chamado: result[0] }, { status: 201 });
-  } catch (error) {
-    console.error("POST error:", error);
-    return NextResponse.json(
-      { error: "Internal server error" },
-      { status: 500 }
-    );
+    const result = await sql`SELECT 1`;
+    console.log("Database connection test:", result);
+  } catch (err) {
+    console.error("Database connection failed:", err);
   }
 }
+
+// Call it when the server starts
+testConnection();
+
+// let isSeeded = false;
 
 export async function GET() {
   try {
-    const chamados = await sql`
-SELECT EXISTS (
-SELECT 1
-FROM information_schema.tables
-WHERE table_name = 'chamados'
-) AS table_existence;
-`;
-
-    return NextResponse.json({ chamados }, { status: 200 });
+    const data = { message: "ok" };
+    return NextResponse.json(data, { status: 201 });
   } catch (error) {
     console.error(error);
+    NextResponse.json({ message: `error: ${error}` }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const formData = await request.json();
+
+    console.log("Received Data:", formData);
+
+    const { ticket, date } = formData;
+
+    const result = await sql`
+      INSERT INTO chamados (ticket, data)
+      VALUES (${ticket}, ${date})
+      RETURNING *
+    `;
+
     return NextResponse.json(
-      { error: "Internal server error" },
-      { status: 500 }
+      {
+        message: "Data received successfully",
+        data: result[0],
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        message: "Error processing request",
+      },
+      {
+        status: 500,
+      }
     );
   }
 }
 
-// export async function GET() {
+// export async function POST(request: Request) {
 //   try {
-//     const chamadosCount = await sql("SELECT COUNT(*) FROM chamados");
-//     if (Number(chamadosCount[0].count) === 0) await seedChamados();
+//     const placeholderData = "10/11/22";
+//     const placeholderTicket = "164489164489";
 
-//     const chamados = await sql("SELECT * FROM chamados");
-//     return NextResponse.json({ chamados }, { status: 200 });
+//     const result = await sql`
+//       INSERT INTO chamados (data, ticket)
+//       VALUES (${placeholderData}, ${placeholderTicket})
+//       RETURNING *
+//     `;
+
+//     return NextResponse.json({ chamado: result[0] }, { status: 201 });
 //   } catch (error) {
-//     console.error(error);
+//     console.error("POST error:", error);
 //     return NextResponse.json(
-//       {
-//         message: "Internal server error",
-//       },
-//       {
-//         status: 500,
-//       }
+//       { error: "Internal server error" },
+//       { status: 500 }
 //     );
 //   }
 // }
 
-const seedChamados = async () => {
-  if (isSeeded) return { message: "Database already seeded", chamados: [] };
+// const seedChamados = async () => {
+//   if (isSeeded) return { message: "Database already seeded", chamados: [] };
 
-  try {
-    const insertChamados = await Promise.all(
-      chamadoPlaceHolder.map(async (chamados) => {
-        const { data, semana, ticket, codigo, status, tipo, link } = chamados;
+//   try {
+//     const insertChamados = await Promise.all(
+//       chamadoPlaceHolder.map(async (chamados) => {
+//         const { data, semana, ticket, codigo, status, tipo, link } = chamados;
 
-        const result = await sql`
-          INSERT INTO chamados (
-            data, semana, ticket, codigo, status, tipo, link
-          ) VALUES (
-            ${data}, ${semana}, ${ticket}, ${codigo}, ${status}, ${tipo}, ${link}
-          ) RETURNING *
-        `;
-        return result[0];
-      })
-    );
+//         const result = await sql`
+//           INSERT INTO chamados (
+//             data, semana, ticket, codigo, status, tipo, link
+//           ) VALUES (
+//             ${data}, ${semana}, ${ticket}, ${codigo}, ${status}, ${tipo}, ${link}
+//           ) RETURNING *
+//         `;
+//         return result[0];
+//       })
+//     );
 
-    isSeeded = true;
+//     isSeeded = true;
 
-    return {
-      message: "Database seeded successfully",
-      chamados: insertChamados,
-    };
-  } catch (error) {
-    console.error(error);
-  }
-};
+//     return {
+//       message: "Database seeded successfully",
+//       chamados: insertChamados,
+//     };
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
